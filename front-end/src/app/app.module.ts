72aad782d51e41a9f3410d7f5cfac3e7ea37d491
gustavomazza <gustavo-mazza@hotmail.com>
2020-10-20 01:32:42 +0000

    (19-10) Adicionada a Toolbar

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './ui/principal/principal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalComponent
+    MainToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
